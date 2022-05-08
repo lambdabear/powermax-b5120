37e60ce3d25e1d851018b69408c9e1aeca43765e
ZhangYi <zhangyi@kideasoft.com>
2022-05-09 00:14:40 +0800

    Support data parse

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use chrono::prelude::*;
+use crc::{Crc, CRC_8_SMBUS};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpListener;
 use tokio::time::{sleep, timeout, Duration};
+
+const CRC_8: Crc<u8> = Crc::<u8>::new(&CRC_8_SMBUS);
 
 // const TCPTIMEOUT: u64 = 10; // seconds
 // const INFLUXDBURL: &str =
@@ -45,6 +48,20 @@ use tokio::time::{sleep, timeout, Duration};
 //     SccAc,
 //     DoNothing,
 // }
+
+fn crc8_check(send: &[u8], rev: &[u8]) -> bool {
+    let mut data = Vec::new();
+    data.extend_from_slice(send);
+    data.extend_from_slice(rev);
+
+    let len = data.len();
+
+    if len > 3 {
+        CRC_8.checksum(&data[..len - 1]) == data[len - 1]
+    } else {
+        false
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -92,12 +109,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             // In a loop, write command to the socket and read the data.
             loop {
                 // Read cells voltage
-                for cmd in 0x01..0x10 {
+                for cmd in 0x01..=0x10 {
                     let data_len = 2;
-                    let msg = [0x0A, cmd, data_len];
-                    println!("Send: {:#X?}", msg);
+                    let send = [0x0A, cmd, data_len];
+                    // println!("Send: {:#X?}", send);
 
-                    if let Err(e) = socket.write_all(&msg).await {
+                    if let Err(e) = socket.write_all(&send).await {
                         eprintln!("failed to write to socket; err = {:?}", e);
                         return;
                     }
@@ -108,10 +125,168 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                         // socket closed
                         Ok(n) if n == 0 => return,
                         Ok(n) => {
-                            println!("REV: {:#X?}", &buf[..n]);
-                            if n == 3 {
-                                let voltage = u16::from_be_bytes(buf[0], buf[1]);
-                                println!("Cell {}: {}mV", cmd, voltage);
+                            // println!("REV: {:#X?}", &buf[..n]);
+                            if n == data_len as usize + 1 {
+                                if crc8_check(&send, &buf[..n]) {
+                                    let voltage = u16::from_be_bytes([buf[0], buf[1]]);
+                                    println!("Cell {}: {}mV", cmd, voltage);
+                                } else {
+                                    println!("CRC-8 checksum error");
+                                }
+                            }
+                        }
+                        Err(e) => {
+                            eprintln!("failed to read from socket; err = {:?}", e);
+                            return;
+                        }
+                    };
+                    sleep(Duration::from_secs(1)).await;
+                }
+
+                // Read temperatures
+                for cmd in 0x13..=0x15 {
+                    let data_len = 2;
+                    let send = [0x0A, cmd, data_len];
+                    // println!("Send: {:#X?}", send);
+
+                    if let Err(e) = socket.write_all(&send).await {
+                        eprintln!("failed to write to socket; err = {:?}", e);
+                        return;
+                    }
+
+                    sleep(Duration::from_secs(1)).await;
+
+                    match socket.read(&mut buf).await {
+                        // socket closed
+                        Ok(n) if n == 0 => return,
+                        Ok(n) => {
+                            // println!("REV: {:#X?}", &buf[..n]);
+                            if n == data_len as usize + 1 {
+                                if crc8_check(&send, &buf[..n]) {
+                                    let temperature = i16::from_be_bytes([buf[0], buf[1]]);
+                                    println!(
+                                        "Temperature {}: {}.{}C°",
+                                        cmd - 0x12,
+                                        temperature / 100,
+                                        temperature % 100
+                                    );
+                                } else {
+                                    println!("CRC-8 checksum error");
+                                }
+                            }
+                        }
+                        Err(e) => {
+                            eprintln!("failed to read from socket; err = {:?}", e);
+                            return;
+                        }
+                    };
+                    sleep(Duration::from_secs(1)).await;
+                }
+
+                // Read total voltage, current, full capacity, remaining capacity
+                for cmd in [0x11, 0x12, 0x16, 0x17] {
+                    let data_len = 4;
+                    let send = [0x0A, cmd, data_len];
+
+                    if let Err(e) = socket.write_all(&send).await {
+                        eprintln!("failed to write to socket; err = {:?}", e);
+                        return;
+                    }
+
+                    sleep(Duration::from_secs(1)).await;
+
+                    match socket.read(&mut buf).await {
+                        // socket closed
+                        Ok(n) if n == 0 => return,
+                        Ok(n) => {
+                            // println!("REV: {:#X?}", &buf[..n]);
+                            if n == data_len as usize + 1 {
+                                if crc8_check(&send, &buf[..n]) {
+                                    match cmd {
+                                        0x11 => {
+                                            let voltage = u32::from_be_bytes([
+                                                buf[0], buf[1], buf[2], buf[3],
+                                            ]);
+                                            println!("Total voltage: {}mV", voltage);
+                                        }
+                                        0x12 => {
+                                            let current = i32::from_be_bytes([
+                                                buf[0], buf[1], buf[2], buf[3],
+                                            ]);
+                                            println!("Current: {}mA", current);
+                                        }
+                                        0x16 => {
+                                            let full_capacity = u32::from_be_bytes([
+                                                buf[0], buf[1], buf[2], buf[3],
+                                            ]);
+                                            println!("Full capacity: {}mAH", full_capacity);
+                                        }
+                                        0x17 => {
+                                            let remaining_capacity = u32::from_be_bytes([
+                                                buf[0], buf[1], buf[2], buf[3],
+                                            ]);
+                                            println!(
+                                                "Remaining capacity: {}mAH",
+                                                remaining_capacity
+                                            );
+                                        }
+                                        _ => (),
+                                    }
+                                } else {
+                                    println!("CRC-8 checksum error");
+                                }
+                            }
+                        }
+                        Err(e) => {
+                            eprintln!("failed to read from socket; err = {:?}", e);
+                            return;
+                        }
+                    };
+                    sleep(Duration::from_secs(1)).await;
+                }
+
+                // Read RSOC, cycle count, pack status, battery status, pack config
+                for cmd in 0x18..=0x1C {
+                    let data_len = 2;
+                    let send = [0x0A, cmd, data_len];
+
+                    if let Err(e) = socket.write_all(&send).await {
+                        eprintln!("failed to write to socket; err = {:?}", e);
+                        return;
+                    }
+
+                    sleep(Duration::from_secs(1)).await;
+
+                    match socket.read(&mut buf).await {
+                        // socket closed
+                        Ok(n) if n == 0 => return,
+                        Ok(n) => {
+                            // println!("REV: {:#X?}", &buf[..n]);
+                            if n == data_len as usize + 1 {
+                                if crc8_check(&send, &buf[..n]) {
+                                    let data = u16::from_be_bytes([buf[0], buf[1]]);
+
+                                    match cmd {
+                                        0x18 => {
+                                            println!("RSOC: {}%", data);
+                                        }
+                                        0x19 => {
+                                            println!("Cycle count: {}", data);
+                                        }
+                                        0x1A => {
+                                            println!("Pack status: {:#04X}", data);
+                                        }
+                                        0x1B => {
+                                            println!("Battery status: {:#04X}", data);
+                                        }
+                                        0x1C => {
+                                            println!("Pack config: {:#04X}", data);
+                                        }
+                                        _ => (),
+                                    }
+                                } else {
+                                    println!("CRC-8 checksum error");
+                                }
                             }
                         }
                         Err(e) => {
